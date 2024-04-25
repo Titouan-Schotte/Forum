fdf0561bd05edfd480cef0c4829c63078a6567f3
Titouan-Schotte <titouan.schotte@gmail.com>
2024-04-25 14:40:59 +0200

    add sqlite base db add create account db func backend add read users db func backend (to fix)

--- main.go
+++ main.go
@@ -1,1 +1,11 @@
-package Forum
+package main
+
+import "fmt"
+
+func main() {
+	db := LoadDb()
+	db.CreateAccount("Titoune", "titouan.scht@gmail.com", "Pass123")
+
+	users, _ := db.GetUsers()
+	fmt.Print(users)
+}
--- structs.go
+++ structs.go
@@ -1,4 +1,4 @@
-package Forum
+package main
 
 type Categorie struct {
 	Id    int
